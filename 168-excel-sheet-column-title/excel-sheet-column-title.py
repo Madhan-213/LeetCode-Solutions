40428619d890509e65e0441945b5c137613933da
Madhan Gowda B.H <mmadhangowdabh@gmail.com>
2026-08-05 22:02:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.79%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = ""
+
+        while columnNumber > 0:
+            columnNumber -= 1
+            reminder = columnNumber % 26
+            result = chr(ord('A') + reminder) + result
+            columnNumber //= 26
+        return result
